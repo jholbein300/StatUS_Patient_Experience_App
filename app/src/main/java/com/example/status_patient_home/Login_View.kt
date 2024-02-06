24ea93cfe375431fc8167e27ae0019a7431b0433
emily <sheth4ever@gmail.com>
2024-02-05 20:42:06 -0500

    Added button click functionality to the login screen and mainActivity screen for logging out

--- app/src/main/java/com/example/status_patient_home/Login_View.kt
+++ app/src/main/java/com/example/status_patient_home/Login_View.kt
@@ -5,16 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Login_View : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_view)
 
-        var logout_button = findViewById<Button>(R.id.logout_button)
-        logout_button.setOnClickListener {
-            val intent = Intent(this, Login_View::class.java)
+        var loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
